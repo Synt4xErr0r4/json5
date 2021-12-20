d99fd663dc3550ead5889f6c9750ed9e575b0b0d
aSemy <897017+aSemy@users.noreply.github.com>
2021-12-20 01:22:44 +0100

    maven release (#5)
    * finish gradle version bump
    
    * try adding maven-publish to make jitpack happy
    
    * fix maven publishing
    
    * bump project version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,25 +4,27 @@ plugins {
   val kotlinVersion = "1.6.10"
   kotlin("jvm") version kotlinVersion
   jacoco
+  `java-library`
+  `maven-publish`
 }
 
 dependencies {
   implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
   val kotlinxSerializationVersion = "1.3.1"
-  implementation(project.dependencies.enforcedPlatform("org.jetbrains.kotlinx:kotlinx-serialization-bom:$kotlinxSerializationVersion"))
+  implementation(platform("org.jetbrains.kotlinx:kotlinx-serialization-bom:$kotlinxSerializationVersion"))
   api("org.jetbrains.kotlinx:kotlinx-serialization-core")
   api("org.jetbrains.kotlinx:kotlinx-serialization-json")
 
   val junitVersion = "5.8.2"
-  testImplementation(enforcedPlatform("org.junit:junit-bom:$junitVersion"))
+  testImplementation(platform("org.junit:junit-bom:$junitVersion"))
   testImplementation("org.junit.jupiter:junit-jupiter")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher") {
     because("Only needed to run tests in a version of IntelliJ IDEA that bundles older versions")
   }
 
   val kotestVersion = "5.0.3"
-  testImplementation(enforcedPlatform("io.kotest:kotest-bom:$kotestVersion"))
+  testImplementation(platform("io.kotest:kotest-bom:$kotestVersion"))
   testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
   testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
   testImplementation("io.kotest:kotest-property:$kotestVersion")
@@ -30,10 +32,11 @@ dependencies {
 }
 
 group = "at.syntaxerror"
-version = "2.0.0"
+version = "2.0.2"
 description = "JSON5 for Kotlin"
 
 java {
+  withJavadocJar()
   withSourcesJar()
 }
 
@@ -92,4 +95,19 @@ tasks.withType<JavaCompile> {
 
 tasks.wrapper {
   gradleVersion = "7.3.2"
+  distributionType = Wrapper.DistributionType.ALL
+}
+
+tasks.javadoc {
+  if (JavaVersion.current().isJava9Compatible) {
+    (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+  }
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("mavenJava") {
+      from(components["java"])
+    }
+  }
 }
